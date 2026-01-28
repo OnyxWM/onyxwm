37b77686d2c8e8e6a8cc7a5688f2b0bf5e735d67
Brodie Owens <brodie@onyxclient.com>
2026-01-29 08:51:17 +1000

    add unstable flag

--- build.sh
+++ build.sh
@@ -3,8 +3,8 @@ set -euo pipefail
 
 root_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-cflags="$(pkg-config --cflags wayland-server wlroots)"
-libs="$(pkg-config --libs wayland-server wlroots)"
+cflags="$(pkg-config --cflags wayland-server wlroots-0.19) -DWLR_USE_UNSTABLE"
+libs="$(pkg-config --libs wayland-server wlroots-0.19)"
 
 clang -c "$root_dir/shim.c" -o "$root_dir/shim.o" $cflags
 odin build "$root_dir" -out:"$root_dir/scumwm" -extra-linker-flags:"$root_dir/shim.o $libs"
